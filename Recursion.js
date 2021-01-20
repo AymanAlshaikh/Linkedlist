1e8cf7122d7b0d8fd22bf4c7d1a6a4acba80b028
AymanAlshaikh <ayman159@live.com>
2021-01-20 21:06:00 +0300

    DONE

--- Recursion.js
+++ Recursion.js
@@ -3,16 +3,18 @@ let numbers = [
   [3, 6, [5, 6], 8, 2, [2, 4], 9],
   [4, 2, [6, 7, [2, 6, 1]]],
 ];
-let total = 0;
+
 function sum(numbers) {
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers.length == 0) {
-      return 0;
-    } else {
-      total = numbers.pop(0) + sum(numbers[i]);
-      return total;
-    }
-  }
+  let total = 0;
+  numbers.forEach((number) => {
+    if (typeof number === "number") total += number;
+    else total += sum(number);
+  });
+  /*for (let i = 0; i <= numbers.length; i++) {
+    if (typeof numbers[i] === "number") total += numbers;
+    else total += sum(numbers);
+  }*/
+  return total;
 }
 let summation = sum(numbers);
 console.log(summation);
